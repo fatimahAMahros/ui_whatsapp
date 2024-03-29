f48fcd83524b131e0c282e2d0caa6b65e59a33cb
fatimahAMahros <fatimah.a.mahros@gmail.com>
2024-03-29 09:55:23 +0700

    first commit

---
+++ lib/models/call.dart
@@ -0,0 +1,16 @@
+class Call {
+  //gambar, nama, date, trailing icon call
+  String name;
+  String callDate;
+
+  Call({required this.name, required this.callDate});
+}
+
+var callList = [
+  Call(name: "Dukhaan", callDate: "10:30 8/3/2024"),
+  Call(name: "Mak", callDate: "09:20 8/3/2024"),
+  Call(name: "Pak", callDate: "15:17 7/3/2024"),
+  Call(name: "Pak Kos", callDate: "14:40 7/3/2024"),
+  Call(name: "Rakha", callDate: "11:20 7/3/2024"),
+  Call(name: "Yasir", callDate: "10:30 7/3/2024"),
+];
---
+++ lib/models/status.dart
@@ -0,0 +1,13 @@
+class Status {
+  String name;
+  String statusDate;
+
+  Status({required this.name, required this.statusDate});
+}
+
+var statusList = [
+  Status(name: "Riro", statusDate: "Now, 10:48 AM"),
+  Status(name: "Diah", statusDate: "Now, 09:39 AM"),
+  Status(name: "Ezra", statusDate: "Now, 07:20 AM"),
+  Status(name: "Maha", statusDate: "Now, 04:20 AM"),
+];
